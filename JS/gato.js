65527543b23b885c7af9a303994f2c2940c7f712
KevinQuesada251 <kquesada988@gmail.com>
2025-02-12 15:44:35 -0600

    Funcion del ganador

--- JS/gato.js
+++ JS/gato.js
@@ -7,7 +7,7 @@ const casilla_6 = document.getElementById("casilla_6")
 const casilla_7 = document.getElementById("casilla_7")
 const casilla_8 = document.getElementById("casilla_8")
 const casilla_9 = document.getElementById("casilla_9")
-
+const btnReiniciar = document.getElementById("btnReiniciar")
 const lista = [casilla_1,casilla_2,casilla_3,casilla_4,casilla_5,casilla_6,casilla_7,casilla_8,casilla_9]
 
 /*
@@ -15,23 +15,42 @@ const lista = [casilla_1,casilla_2,casilla_3,casilla_4,casilla_5,casilla_6,casil
     para luego usar una función y darles el evento que dibuje la X
 */
 
-function marcarCasillas() {
+function marcarCasillas() {                              /*F*/ 
     lista.forEach((item)=>item.addEventListener("click",function(){
-        item.innerHTML = "x"
-        marcaCirculo()
+        if (item.innerHTML === "" && !elegirGanador()) {
+            item.innerHTML = "X" 
+            marcaCirculo()
+            elegirGanador()
+        }
     }))
 }
 
 function marcaCirculo() {
-
     const casillasVacias = lista.filter(vacia=> vacia.innerHTML == "")
     const posicionAleatoria = Math.floor(Math.random() * casillasVacias.length);
     if (posicionAleatoria != casillasVacias) {
-
         casillasVacias[posicionAleatoria].innerHTML="O"
-        console.log(posicionAleatoria);
     }
 }
 
+function elegirGanador(){
+    let posicionesGanadoras = [
+        [0,1,2],[3,4,5],[6,7,8], // FILAS
+        [0,2,6],[1,4,7],[2,5,8], // COLUMNAS
+        [0,4,8],[2,4,6] // DIAGONALES
+    ]
+    
+    for (const iterar of posicionesGanadoras) {
+        let [pos1,pos2,pos3] = iterar
+        if (lista[pos1].textContent != "" && 
+            lista[pos1].textContent === lista[pos2].textContent && 
+            lista[pos1].textContent === lista[pos3].textContent) {
+            alert(`El ganador es: ${lista[pos1].textContent}`)
+            return true 
+        }
+    } 
+    
+}
 
 marcarCasillas()
+
